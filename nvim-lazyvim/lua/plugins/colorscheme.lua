d57e77ae1766ba760eb80ca58a43b553a18ee58f
Jens Nilsson <jenszza@gmail.com>
2026-01-18 13:10:21 +0100

    colorscheme and todo highlighting
    extend todo comments pattern matching

--- nvim-lazyvim/lua/plugins/colorscheme.lua
+++ nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,27 @@
 return {
   {
-    "catppuccin",
+    "catppuccin/nvim",
+    name = "catppuccin",
     opts = {
-      transparent_background = true,
+      -- transparent_background = true,
       flavour = "mocha",
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = true,
+      float = {
+        transparent = true,
+      },
+      -- term_colors = true,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-      -- transparent = true,
+      transparent = true,
     },
   },
 }
